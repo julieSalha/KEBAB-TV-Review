912e6831fd45bff1aaa44ccb9ca072c57f852c5c
«julieSalha» <julie.salha@gmail.com>
2020-03-19 16:57:30 +0100

    consts

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@
     const themoviedbUrl = 'https://api.themoviedb.org/3/search/movie?api_key=6fd32a8aef5f85cabc50cbec6a47f92f&query=';
 
         // API Node Back-end Julien NOYER
-    const apiUrl = 'https://api.dwsapp.io'; //Register/login/favori (API Node)
+    const apiUrl = 'https://kebabtv.dwsapp.io'; //Register/login/favori (API Node)
 
         // Local Storage
     const localSt = 'fzfzhfskd';
